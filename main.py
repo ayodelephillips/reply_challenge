f21572d6643a852f35d8707c4f319ecc91249db9
Adewunmi Aboyade-cole <67490387+geekwunmi@users.noreply.github.com>
2023-05-13 20:27:42 +0100

    added file reader

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os as os
+
+def read_file():
+    base_path = f"{os.path.normpath(os.getcwd() + os.sep + os.pardir)}"
+    input_file = f"{base_path}/reply_challenge/input_File.txt"
+    colnames = ['Enemy']
+    file_import_df = pd.read_csv(input_file
+                             ,header = None
+                            , names = colnames
+                             )
+    # print(file_import_df['Enemy'].loc[])
+    # print(file_import_df[2])
+    return file_import_df
+read_file()
+# print(file_import_df.head(9))
+# file_import_df = file_import_df.replace(' ',',', regex = True)
+# print(file_import_df.head(10))
