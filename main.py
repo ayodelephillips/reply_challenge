a903e16975a600937db2817e8ccf429987277e30
Adewunmi Aboyade-cole <67490387+geekwunmi@users.noreply.github.com>
2023-07-03 23:08:57 +0100

    Ade simulation logic update 20230703

--- main.py
+++ main.py
@@ -41,16 +41,42 @@ class Simulation:
         Stamina is deducted each turn then added back after
         defeating the enemy
          """
-        p_stamina = int(self.starting_stamina_pt)
-        e_stamina = self.enemies_details.iloc[0,0].astype('int')
+        global stamina_recovery_turns
+        global stamina_recovery
 
-        if p_stamina >= e_stamina:
-           p_stamina -= e_stamina
-           stamina_recovery_turns = self.enemies_details.iloc[0,1]
-           stamina_recovery = self.enemies_details.iloc[0,2]
-        else:
-            # return turns_rule_set()
-            pass
+        p_stamina = int(self.starting_stamina_pt)
+        e_details = self.enemies_details.iloc[0].to_frame()
+        e_stamina = int(e_details.iloc[0,0][0:2])
+
+        # print(e_stamina)
+        # print(len(self.enemies_details))
+
+        for x in self.enemies_details:
+            enemy_faced = 0
+            stamina_recovery = 0
+            stamina_recovery_turns = 0
+
+            if p_stamina <= e_stamina:
+                p_stamina -= e_stamina
+                stamina_recovery_turns += int(self.enemies_details.iloc[0][0][4:])
+                stamina_recovery += int(self.enemies_details.iloc[0][0][0])
+                enemy_faced += 1
+
+                print('hi')
+                print(stamina_recovery)
+                print(stamina_recovery_turns)
+            else:
+                print('hello')
+                print(e_stamina)
+                print(p_stamina)
+                print(stamina_recovery)
+                print(stamina_recovery_turns)
+                return self.enemies_details
+
+            return stamina_recovery_turns, stamina_recovery
+            print('hey')
+            # print(stamina_recovery)
+            # print(stamina_recovery_turns)
 
 
     def turns_rule_set(self, p_turns, e_turns_to_beat):
@@ -62,6 +88,9 @@ class Simulation:
         in each turn you can revoer all stamina from defested demons
         fragment collecting will last a maximum of t runs
         A variable is stored to count total turns"""
+
+        # for x in range(p_turns):
+        #     if stamina_recovery_turns ==
 
 
         pass
@@ -80,4 +109,5 @@ class Simulation:
 if __name__ == "__main__":
     simulation = Simulation()
     simulation.run()
-    # simulation.stamina_rule_set()
+    simulation.stamina_rule_set()
+    # simulation.turns_rule_set()
